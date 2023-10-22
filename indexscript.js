4c6c70e5a467c8168505c48c58f678812ebb9bbd
The4thMusketeer <kwakuanor18@gmail.com>
2023-10-22 17:39:39 +0000

    init

---
+++ indexscript.js
@@ -0,0 +1,11 @@
+// Get the button element from the DOM
+const button = document.querySelector('button');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    // Add your functionality here
+    console.log('Button clicked!');
+});
+
+
+window.alert("Hello World")
